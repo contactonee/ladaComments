97e6a91b2ed55e0aa6dbce14df493d208ad2b8e2
Ruslan Bazhenov <ruslan.aktau@gmail.com>
2016-05-23 02:24:08 +0500

    Make output dict

--- core.py
+++ core.py
@@ -11,13 +11,24 @@ class MyHTMLParser(HTMLParser):
         for val in attrs:
             attr[val[0]] = val[1]
         self.inside.append((tag, attr))
+        if 'class' in attr and attr['class'] == 'comment-item':
+            comments.append({'text': '', 'author': '', 'title': '', 'date': '', 'id': ''})
         if tag == 'div' and 'class' in attr and attr['class'] == 'comment-text':
             self.inComment = 1
-            comments.append("")
+            
+
     def handle_data(self, data):
-      if len(self.inside) > 0:
         if self.inComment:
-            comments[-1] += data.replace("*", "")
+            comments[-1]['text'] += data.replace("*", "")
+            if 'id' in self.inside[-1][1] and self.inside[-1][1]['id'][0:7] == 'comm-id':
+                comments[-1]['id'] = self.inside[-1][1]['id'][8:]
+        if len(self.inside) >= 2:
+            if 'class' in self.inside[-2][1] and self.inside[-2][1]['class'] == 'comment-user' and self.inside[-1][0] == 'a':
+                comments[-1]['author'] = data
+            if self.inside[-2][0] == 'h3' and self.inside[-1][0] == 'a':
+                comments[-1]['title'] = data
+            if 'class' in self.inside[-1][1] and self.inside[-1][1]['class'] == 'comment-date':
+                comments[-1]['date'] = data
     def handle_endtag(self, tag):
         if len(self.inside) > 0:
             if self.inside[-1][0] == 'div' and 'class' in self.inside[-1][1] and self.inside[-1][1]['class'] == 'comment-text':
@@ -29,5 +40,3 @@ class MyHTMLParser(HTMLParser):
 parser = MyHTMLParser()
 res = urllib.urlopen("https://www.lada.kz/index.php?do=lastcomments").read().decode("utf-8").replace("--!>", "-->")
 parser.feed(res)
-for text in comments:
-    print text.encode("utf-8"), "\n\n"
